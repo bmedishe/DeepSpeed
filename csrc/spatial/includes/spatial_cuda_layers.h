3cb8e5a5428dff8f63df152d0406e4c09192d77f
bmedishe <bmedishe@amd.com>
2023-07-07 20:35:01 +0000

    stable_diffusion inference with deepspeed on rocm

--- csrc/spatial/includes/spatial_cuda_layers.h
+++ csrc/spatial/includes/spatial_cuda_layers.h
@@ -8,8 +8,11 @@
 #if __CUDA_ARCH__ >= 530
 #define HALF_PRECISION_AVAILABLE = 1
 #endif
-
+#ifdef __HIP_PLATFORM_HCC__
+#include <hip/hip_cooperative_groups.h>
+#else
 #include <cooperative_groups.h>
+#endif
 #include <cuda.h>
 #include <cuda_fp16.h>
 
